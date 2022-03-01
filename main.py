9a505f7bdd2a30faaff773bcf91fac95975ab65c
hossvadi <hossvadi@gmail.com>
2022-03-02 00:52:29 +0100

    call

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from pathlib import Path
+from re import I
+from utils import read_json
+from data import DATA_DIR
 
 import shutil
 
-class OrganizeDir:
-    suffix_dir = {
-            '.png': 'images',
-            '.PNG': 'images',
-            '.jpeg': 'images',
-            '.jpg': 'images',
-            '.pdf': 'pdf',
-            '.docx': 'word',
-            '.csv': 'databace',
-            '.sql': 'databace',
-            '.zip': 'compressed',
-            '.rar': 'compressed',
-            '.deb': 'exe',
-            '.app': 'exe',
-            '.exe': 'exe',
-            '.dmg': 'docs',
-            '.mp4': 'video',
-    }
 
-    def run(self, to_organize_dir):
+class OrganizeDir:
+    """Organizing files in a given directory
+
+    Example:
+    >>> organizer = organizeDir()
+    >>> organizer.run(to_organize_directory)
+    """
+
+    suffix_dir =  read_json(DATA_DIR / 'suffix_dir.json')
+
+    def __call__(self, to_organize_dir: str):
+        """Runs organizer jo b in order to move files to the specific destinations
+
+        :param to_organize_dir: directiry to run organization job
+        """
         self.to_organize_dir = Path(to_organize_dir).resolve()
         assert self.to_organize_dir.exists(), f'Directory "{self.to_organize_dir}" not found!'
 
+        # iterate over files t organize
         for path in self.to_organize_dir.iterdir():
 
             if path.is_dir():
-                if path.name in OrganizeDir.values():
+                # to avoid moving directories created by us
+                if path.name in OrganizeDir.suffix_dir.values():
                     continue
 
                 dest = 'directory'
@@ -38,8 +39,9 @@ class OrganizeDir:
 
             elif path.is_file():
                 suffix = path.suffix
-                dest = OrganizeDir.get(suffix)
+                dest = OrganizeDir.suffix_dir.get(suffix)
 
+                # suffix is no supported yet
                 if not dest:
                     continue
 
@@ -50,4 +52,4 @@ class OrganizeDir:
 if __name__ == '__main__':
     HOME_DIR = Path.home()
     organizer = OrganizeDir()
-    organizer.run(HOME_DIR / 'Downloads')
+    organizer(HOME_DIR / 'Downloads')
